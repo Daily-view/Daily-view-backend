244e9b1ac393bf7d095b14422e16f8f289a7e08f
nothingprogram <popawaw@naver.com>
2022-08-15 19:17:40 +0900

    feat: security 추가 및 jwt 관련 코드 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -22,12 +22,21 @@ val logstashLogbackVersion: String? by ext
 
 dependencies {
     api(project(":domain"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:latest.release"))
     implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter")
     implementation("com.netflix.graphql.dgs:graphql-dgs-extended-scalars")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("net.logstash.logback:logstash-logback-encoder:7.2")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
+
+    // jwt
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+    implementation("org.springframework.boot:spring-boot-starter-security:2.7.2")
+
     testImplementation(testFixtures(project(":domain")))
     testImplementation("com.netflix.graphql.dgs:graphql-dgs-client")
 }
---
+++ api/src/main/kotlin/com/dailyview/api/configuration/security/WebSecurityConfig.kt
@@ -0,0 +1,56 @@
+package com.dailyview.api.configuration.security
+
+import com.dailyview.api.filter.JwtFilter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+@EnableWebSecurity
+open class WebSecurityConfig(
+    private val jwtFilter: JwtFilter
+) : WebSecurityConfigurerAdapter() {
+
+    @Bean
+    fun defaultPasswordEncoder(): BCryptPasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    override fun authenticationManagerBean(): AuthenticationManager {
+        return super.authenticationManagerBean()
+    }
+
+    @Bean
+    fun configurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.addAllowedHeader("*")
+        configuration.addAllowedOriginPattern("*")
+        configuration.addAllowedMethod("*")
+        configuration.allowCredentials = true
+        configuration.maxAge = 3600L
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
+    override fun configure(http: HttpSecurity?) {
+        http?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http?.logout()?.disable()?.formLogin()?.disable()?.httpBasic()?.disable()
+
+        http?.cors()?.configurationSource(configurationSource())
+            ?.and()?.csrf()?.disable()?.authorizeRequests()?.anyRequest()?.permitAll()
+
+        http?.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
+    }
+}
