20330bc6294633cd5e2626a59b3e6c131dacc00c
nothingprogram <popawaw@naver.com>
2022-08-20 20:47:58 +0900

    feat: firebase fileupload

--- api/src/main/kotlin/com/dailyview/api/fetcher/FileUploadDataFetcher.kt
+++ api/src/main/kotlin/com/dailyview/api/fetcher/FileUploadDataFetcher.kt
@@ -1,15 +1,24 @@
 package com.dailyview.api.fetcher
 
 import com.dailyview.api.generated.DgsConstants
+import com.dailyview.api.service.fb.FirebaseService
 import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsMutation
 import com.netflix.graphql.dgs.InputArgument
+import java.util.UUID
 import org.springframework.web.multipart.MultipartFile
 
 @DgsComponent
-class FileUploadDataFetcher {
+class FileUploadDataFetcher(val firebaseService: FirebaseService) {
 
     @DgsMutation(field = DgsConstants.MUTATION.UploadImage)
-    fun upload(@InputArgument input: MultipartFile) {
+    fun upload(@InputArgument input: MultipartFile): String {
+        return firebaseService.uploadFiles(file = input, fileName = generateUUIDFileName(file = input))
     }
+}
+
+fun generateUUIDFileName(file: MultipartFile): String {
+    val fileName = file.name
+    val ext = fileName.substring(fileName.lastIndexOf(".") + 1)
+    return "${UUID.randomUUID()}.$ext"
 }
