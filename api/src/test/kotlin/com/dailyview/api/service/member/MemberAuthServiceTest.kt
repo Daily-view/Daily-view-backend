2ed70dc529e2683c3c918f3579ff75cd18c5d957
neimen-95 <neimen.95@kakaostyle.com>
2022-08-20 16:19:12 +0900

    test code 깨지는것 수정

--- api/src/test/kotlin/com/dailyview/api/service/member/MemberAuthServiceTest.kt
+++ api/src/test/kotlin/com/dailyview/api/service/member/MemberAuthServiceTest.kt
@@ -1,29 +1,28 @@
 package com.dailyview.api.service.member
 
+import com.dailyview.api.configuration.security.JwtTokenProvider
 import com.dailyview.api.exception.BusinessException
 import com.dailyview.api.exception.ErrorCode
 import com.dailyview.api.generated.types.LoginInput
+import com.dailyview.api.service.auth.RefreshTokenService
 import com.dailyview.domain.entity.MemberRepository
+import com.dailyview.domain.entity.redis.MemberRefreshTokenRepository
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 class MemberAuthServiceTest : BehaviorSpec({
     val memberRepository: MemberRepository = mockk()
-    val encoder: BCryptPasswordEncoder = mockk()
-    val memberAuthService = MemberAuthService(encoder, memberRepository)
+    val jwtTokenProvider: JwtTokenProvider = mockk()
+    val refreshTokenService: RefreshTokenService = mockk()
+    val refreshTokenRepository: MemberRefreshTokenRepository = mockk()
+    val authenticationManagerBuilder: AuthenticationManagerBuilder = mockk()
+    val memberAuthService = MemberAuthService(
+        authenticationManagerBuilder, jwtTokenProvider, refreshTokenService, memberRepository, refreshTokenRepository
+    )
 
-    Given("Setup login input") {
-        val loginInput = LoginInput(email = "popawaw@naver.com", password = "12345678")
-        When("email 을 찾을수 없어서") {
-            every { memberRepository.findByEmail(loginInput.email) } returns null
-            Then("예외가 발생한다.") {
-                val result = shouldThrow<BusinessException> { memberAuthService.login(loginInput) }
-                result.errorCode shouldBe ErrorCode.MEMBER_DOES_NOT_EXISTS
-            }
-        }
-    }
 })
