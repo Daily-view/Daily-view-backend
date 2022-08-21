681856515586098827770a1921329b0dc3f66605
nothingprogram <popawaw@naver.com>
2022-08-21 20:55:43 +0900

    webSecurityAdapter 레거시 코드 제거

--- api/src/main/kotlin/com/dailyview/api/configuration/security/WebSecurityConfig.kt
+++ api/src/main/kotlin/com/dailyview/api/configuration/security/WebSecurityConfig.kt
@@ -1,24 +1,26 @@
 package com.dailyview.api.configuration.security
 
 import com.dailyview.api.filter.JwtFilter
+import com.dailyview.api.service.member.UserDetailService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
-class WebSecurityConfig(
-    private val jwtFilter: JwtFilter
-) : WebSecurityConfigurerAdapter() {
+class WebSecurityConfig(private val jwtFilter: JwtFilter) {
 
     @Bean
     fun defaultPasswordEncoder(): BCryptPasswordEncoder {
@@ -26,31 +28,42 @@ class WebSecurityConfig(
     }
 
     @Bean
-    override fun authenticationManagerBean(): AuthenticationManager {
-        return super.authenticationManagerBean()
+    fun authManger(
+        http: HttpSecurity,
+        bCryptPasswordEncoder: BCryptPasswordEncoder,
+        userDetailService: UserDetailService
+    ): AuthenticationManager {
+        return http.getSharedObject(AuthenticationManagerBuilder::class.java)
+            .userDetailsService(userDetailService)
+            .passwordEncoder(bCryptPasswordEncoder)
+            .and()
+            .build()
     }
 
     @Bean
-    fun configurationSource(): CorsConfigurationSource {
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf().disable()
+            .headers().frameOptions().disable().and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .cors().configurationSource(corsConfigurationSource()).and()
+            .authorizeRequests().anyRequest().permitAll()
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
+        return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource? {
         val configuration = CorsConfiguration()
-        configuration.addAllowedHeader("*")
-        configuration.addAllowedOriginPattern("*")
-        configuration.addAllowedMethod("*")
+        configuration.addAllowedHeader(CorsConfiguration.ALL)
+        configuration.addAllowedOriginPattern(CorsConfiguration.ALL)
+        configuration.addAllowedMethod(CorsConfiguration.ALL)
         configuration.allowCredentials = true
         configuration.maxAge = 3600L
-
         val source = UrlBasedCorsConfigurationSource()
         source.registerCorsConfiguration("/**", configuration)
         return source
-    }
-
-    override fun configure(http: HttpSecurity?) {
-        http?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        http?.logout()?.disable()?.formLogin()?.disable()?.httpBasic()?.disable()
-
-        http?.cors()?.configurationSource(configurationSource())
-            ?.and()?.csrf()?.disable()?.authorizeRequests()?.anyRequest()?.permitAll()
-
-        http?.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
     }
 }
