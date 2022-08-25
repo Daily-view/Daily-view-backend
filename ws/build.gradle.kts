83f83d7a83652baf8857ae762ed26448266d8482
nothingprogram <popawaw@naver.com>
2022-08-25 15:03:31 +0900

    set up websocket module

---
+++ ws/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+}
+
+group = "com.dailview"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	api(project(":domain"))
+	implementation("org.springframework.boot:spring-boot-starter-integration:2.7.3")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+}
